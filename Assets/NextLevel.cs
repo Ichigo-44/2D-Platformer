67cbba5f86684956f9cd8340996cc3babb72aff9
Ichigo-44 <darek8514@gmail.com>
2026-04-30 15:09:05 +0200

    kolejne zmiany

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+
+public class NextLevel : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
